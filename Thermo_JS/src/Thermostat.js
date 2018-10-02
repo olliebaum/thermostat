e2ff40f0266a26c8bc3a5a5b9cfe1d55757c4656
NicGiles <n.r.giles1@gmail.com>
2018-10-02 16:33:22 +0100

    Add #up + #down methods and implement minimum temperature of 10

--- Thermo_JS/src/Thermostat.js
+++ Thermo_JS/src/Thermostat.js
@@ -3,3 +3,15 @@ var Thermostat = function() {
   this.temperature = 20;
 
 };
+
+Thermostat.prototype.up = function(number) {
+  this.temperature += number;
+};
+
+Thermostat.prototype.down = function(number) {
+  if (this.temperature - number < 10 ) {
+    this.temperature = 10;
+  } else {
+    this.temperature -= number;
+  }
+};
